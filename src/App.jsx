7a0d75388d8821ea6eddccfc372dc04d6b2df49c
CynthiaStephan <cynthia.stephan@institusolacroup.com>
2024-11-10 18:15:25 +0100

    mobile scroll fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,27 @@ import About from './pages/About'
 import EliseAmar from './content/EliseAmar'
 import Dailyfy from './content/Dailyfy'
 import Fgodf from './content/Fgodf'
-import { ReactLenis } from 'lenis/react'
-
+import { ReactLenis, useLenis } from 'lenis/react'
 import MentionsLegales from './pages/mentionsLegales'
 
     function App() {
-        // const lenis = useLenis(({ scroll }) => {
-        //     // called every scroll
-        //   });
+         const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  })
 
 
           
     return (
-        <>
-            <ReactLenis root>
+
+        <ReactLenis
+            root
+            options={{
+            autoRaf: true,
+            smoothTouch: true,
+            direction: "vertical", // Spécifie le sens du défilement
+            gestureDirection: "vertical", // Précise le geste pour le défilement tactile
+            }}
+        >
             
             <div className='background-columns'>
                 <div className='column'></div>
@@ -30,24 +37,26 @@ import MentionsLegales from './pages/mentionsLegales'
                 <div className='column '></div>
                 <div className='column desktop'></div>
             </div>
-                <BrowserRouter>
-                    <Routes>
-                        <Route element={<Homepage />} path="/" />
-                        <Route element={<Projects />} path="/projets" />
-                        <Route element={<About />} path='/apropos' />
-                        <Route element={<MentionsLegales />} path='/legal' />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Homepage />} path="/" />
+                    <Route element={<Projects />} path="/projets" />
+                    <Route element={<About />} path='/apropos' />
+                    <Route element={<MentionsLegales />} path='/legal' />
 
 
-                        <Route element={<MiamApp />} path='/projets/miamapp' />
-                        <Route element={<EliseAmar />} path='/projets/eliseamar' />
-                        <Route element={<Dailyfy />} path='/projets/dailyfy' />
-                        <Route element={<Fgodf />} path='/projets/fgodf' />
-                        
-                    </Routes>
-                </BrowserRouter>
-            </ReactLenis>
+                    <Route element={<MiamApp />} path='/projets/miamapp' />
+                    <Route element={<EliseAmar />} path='/projets/eliseamar' />
+                    <Route element={<Dailyfy />} path='/projets/dailyfy' />
+                    <Route element={<Fgodf />} path='/projets/fgodf' />
+                    
+                </Routes>
+            </BrowserRouter>
 
-        </>
+        </ReactLenis>
+
+
     )
     }
 
