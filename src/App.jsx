f3250f7aac8e8f3ee0aff78b729518905e220637
CynthiaStephan <s.cynthia972@gmail.com>
2026-08-08 19:08:43 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
                 <div className='column desktop'></div>
             </div>
 
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <Routes>
                     <Route element={<Homepage />} path="/" />
                     <Route element={<Projects />} path="projets" />
